fcd51f1bf67d1f60df32d1a307cfac035f671501
nataliapst <79333938+nataliapst@users.noreply.github.com>
2025-06-30 14:08:50 +0200

    Update HCOPY.bat

--- HCOPY.bat
+++ HCOPY.bat
@@ -1,6 +1,6 @@
 @echo off
 
-HCopy -T 1 -C C:\Users\root\Desktop\htk-3.2.1\config.txt -S C:\Users\root\Desktop\htk-3.2.1\wav_to_mfc_paths_CORRECT.txt
+HCopy -T 1 -C C:\Users\root\Desktop\htk-3.2.1\config.txt -S C:\Users\root\Desktop\htk-3.2.1\wav_to_mfc_paths.txt
 
 endlocal
 echo Proceso completado.
