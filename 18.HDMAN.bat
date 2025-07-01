fd0a07fa7a73ca7d87609ea360066dd61080cde4
nataliapst <79333938+nataliapst@users.noreply.github.com>
2025-07-01 13:20:48 +0200

    Update 18.HDMAN.bat

--- 18.HDMAN.bat
+++ 18.HDMAN.bat
@@ -1,7 +1,7 @@
 #CREATION OF dict.dict
 @echo off
 
-HDMan -m -w  C:\Users\root\Desktop\htk-3.2.1\dict.dict  C:\Users\root\Desktop\htk-3.2.1\clases2.list
+HDMan -m -w  C:\Users\root\Desktop\htk-3.2.1\clases2.list C:\Users\root\Desktop\htk-3.2.1\dict.dict  C:\Users\root\Desktop\htk-3.2.1\clases2.list
 
 endlocal
 echo Proceso completado.
