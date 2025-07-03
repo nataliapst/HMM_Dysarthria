054b0c9c65facb6a44562e2f29cdc837da693997
nataliapst <79333938+nataliapst@users.noreply.github.com>
2025-07-03 11:02:08 +0200

    Update to 18.HPARSE.bat

--- 18.HDMAN.bat
+++
@@ -1,8 +0,0 @@
-#CREATION OF dict.dict
-@echo off
-
-HDMan -m -w  C:\Users\root\Desktop\htk-3.2.1\clases2.list C:\Users\root\Desktop\htk-3.2.1\dict.dict  C:\Users\root\Desktop\htk-3.2.1\clases2.list
-
-endlocal
-echo Proceso completado.
-pause
---
+++ 18.HPARSE.bat
@@ -0,0 +1,7 @@
+#CREATION OF wdnet1
+@echo off
+
+HParse grammar.txt wdnet1
+
+endlocal
+pause
